7d16b868e2509275e7b67a2153baa5b0e7c396d9
Pieter Provoost <pieterprovoost@gmail.com>
2024-08-07 14:56:56 +0200

    take into account missing layers

--- index.js
+++ index.js
@@ -8,88 +8,85 @@ const speedyMap = (config) => {
     });
     
     map.on("load", () => {
-    
-        map.addSource("density", {
-            type: "geojson",
-            data: config.sources.density
-        });
-    
-        map.addSource("summary", {
-            type: "geojson",
-            data: config.sources.summary
-        });
-    
-        map.addSource("envelope", {
-            type: "geojson",
-            data: config.sources.envelope
-        });
-    
-        map.addSource("distribution", {
-            type: "geojson",
-            data: config.sources.distribution
-        });
-    
-        map.addLayer({
-            "id": "envelope",
-            "type": "fill",
-            "source": "envelope",
-            "paint": {
-                "fill-color": "rgb(146, 181, 85)",
-                "fill-opacity": 0.5
+
+        ["density", "summary", "envelope", "distribution"].forEach((source) => {
+            if (config.sources[source]) {
+                map.addSource(source, {
+                    type: "geojson",
+                    data: config.sources[source]
+                });
             }
         });
     
-        map.addLayer({
-            "id": "density",
-            "type": "fill",
-            "source": "density",
-            "paint": {
-                "fill-color": [
-                    "interpolate",
-                    ["linear"],
-                    ["get", "density"],
-                    0, "#008080",
-                    0.000025, "#70a494",
-                    0.00005, "#b4c8a8",
-                    0.0001, "#f6edbd",
-                    0.0002, "#edbb8a",
-                    0.0004, "#de8a5a",
-                    0.0008, "#ca562c"
+        if (config.sources.envelope) {
+            map.addLayer({
+                "id": "envelope",
+                "type": "fill",
+                "source": "envelope",
+                "paint": {
+                    "fill-color": "rgb(146, 181, 85)",
+                    "fill-opacity": 0.5
+                }
+            });
+        }
     
-                ],
-                "fill-opacity": 0.5
-            }
-        });
+        if (config.sources.density) {
+            map.addLayer({
+                "id": "density",
+                "type": "fill",
+                "source": "density",
+                "paint": {
+                    "fill-color": [
+                        "interpolate",
+                        ["linear"],
+                        ["get", "density"],
+                        0, "#008080",
+                        0.000025, "#70a494",
+                        0.00005, "#b4c8a8",
+                        0.0001, "#f6edbd",
+                        0.0002, "#edbb8a",
+                        0.0004, "#de8a5a",
+                        0.0008, "#ca562c"
         
-        map.addLayer({
-            "id": "summary",
-            "type": "fill",
-            "source": "summary",
-            "paint": {
-                "fill-color": [
-                    "match",
-                    ["get", "establishmentMeans"],
-                    "native", "rgb(171, 196, 147)",
-                    "introduced", "rgb(245, 66, 93)",
-                    "uncertain", "rgb(202, 117, 255)",
-                    "none", "rgb(237, 167, 69)",
-                    "rgb(237, 167, 69)"
-                ],
-                "fill-opacity": 0.5
-            }
-        });
-    
-        map.addLayer({
-            "id": "distribution",
-            "type": "circle",
-            "source": "distribution",
-            "paint": {
-                "circle-radius": 2,
-                "circle-opacity": 0,
-                "circle-stroke-width": 1.2,
-                "circle-stroke-color": "#000"
-            }
-        });
+                    ],
+                    "fill-opacity": 0.5
+                }
+            });
+        }
+        
+        if (config.sources.summary) {
+            map.addLayer({
+                "id": "summary",
+                "type": "fill",
+                "source": "summary",
+                "paint": {
+                    "fill-color": [
+                        "match",
+                        ["get", "establishmentMeans"],
+                        "native", "rgb(171, 196, 147)",
+                        "introduced", "rgb(245, 66, 93)",
+                        "uncertain", "rgb(202, 117, 255)",
+                        "none", "rgb(237, 167, 69)",
+                        "rgb(237, 167, 69)"
+                    ],
+                    "fill-opacity": 0.5
+                }
+            });
+        }
+
+        if (config.sources.distribution) {
+            map.addLayer({
+                "id": "distribution",
+                "type": "circle",
+                "source": "distribution",
+                "paint": {
+                    "circle-radius": 2,
+                    "circle-opacity": 0,
+                    "circle-stroke-width": 1.2,
+                    "circle-stroke-color": "#000"
+                }
+            });
+        }
 
     });
 
@@ -98,42 +95,45 @@ const speedyMap = (config) => {
         const toggleableLayerIds = ["density", "summary", "envelope", "distribution"];
 
         for (const id of toggleableLayerIds) {
-            if (document.getElementById(id)) {
-                continue;
-            }
+            if (config.sources[id]) {
 
-            const link = document.createElement("a");
-            link.id = id;
-            link.href = "#";
-            link.textContent = id;
-            link.className = "active";
-
-            link.onclick = function (e) {
-                const clickedLayer = this.textContent;
-                e.preventDefault();
-                e.stopPropagation();
-
-                const visibility = map.getLayoutProperty(
-                    clickedLayer,
-                    "visibility"
-                );
-
-                if (visibility === undefined || visibility === "visible") {
-                    map.setLayoutProperty(clickedLayer, "visibility", "none");
-                    this.className = "";
-                } else {
-                    this.className = "active";
-                    map.setLayoutProperty(
-                        clickedLayer,
-                        "visibility",
-                        "visible"
-                    );
+                if (document.getElementById(id)) {
+                    continue;
                 }
-            };
-
-            const layers = document.getElementById("menu");
-            layers.appendChild(link);
+    
+                const link = document.createElement("a");
+                link.id = id;
+                link.href = "#";
+                link.textContent = id;
+                link.className = "active";
+    
+                link.onclick = function (e) {
+                    const clickedLayer = this.textContent;
+                    e.preventDefault();
+                    e.stopPropagation();
+    
+                    const visibility = map.getLayoutProperty(
+                        clickedLayer,
+                        "visibility"
+                    );
+    
+                    if (visibility === undefined || visibility === "visible") {
+                        map.setLayoutProperty(clickedLayer, "visibility", "none");
+                        this.className = "";
+                    } else {
+                        this.className = "active";
+                        map.setLayoutProperty(
+                            clickedLayer,
+                            "visibility",
+                            "visible"
+                        );
+                    }
+                };
+    
+                const layers = document.getElementById("menu");
+                layers.appendChild(link);
+            }
         }
-    });
 
+    });
 }
